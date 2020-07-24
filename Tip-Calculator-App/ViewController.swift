238671d4e07bc6b12594ae630ffaa4ab6cf30355
tanay3547 <49569156+tanay3547@users.noreply.github.com>
2020-07-24 19:43:58 +0530

    Initial Commit

---
+++ Tip-Calculator-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip-Calculator-App
+//
+//  Created by Tanay Agrawal on 7/24/20.
+//  Copyright © 2020 Tanay Agrawwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
